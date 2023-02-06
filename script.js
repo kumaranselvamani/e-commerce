72c18a0275a95c7029b1c6ed580d040cf1d15108
Kumaran S <123823709+kumaranselvamani@users.noreply.github.com>
2023-02-06 13:37:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,12 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
+
+function bootstrapAlert() {
+  alert("Site is under developing!");
+}
+
+
+function calc(){
+  alert("you can't buy now Site is under developing!");
+}
